c4d3284104466d357907082d59fffe76bf1eccf7
Jabez Victor Chavez <jabezvictor05@gmail.com>
2023-11-29 13:14:22 +0800

    ajkhdaskjh

--- VotingSystem/VotingSystem/settings.py
+++ VotingSystem/VotingSystem/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nvhd*q53*y-e+6^h#3*$9tm2wnrle5zqwm^st%h6dqkcxtxjdi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -129,7 +129,7 @@ USE_TZ = True
 
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/' 
 
 
